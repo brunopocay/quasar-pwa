f66fb7c3b26dc3951e5ad5b53f854899de945508
Bruno Pocay <brunopocay@gmail.com>
2024-07-12 10:07:04 -0300

    teste de componentens 2

--- src/components/CameraComponent.vue
+++ src/components/CameraComponent.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <q-btn @click="startCamera">Abrir Câmera</q-btn>
-    <q-btn @click="startRecording">Gravar Áudio</q-btn>
+    <q-btn @click="startRecordingVideo">Gravar Vídeo</q-btn>
+    <q-btn @click="stopRecordingVideo" :disabled="!isRecording">Parar Gravação</q-btn>
     <video ref="video" autoplay></video>
+    <video ref="recordedVideo" controls></video>
+    <q-btn @click="startRecording">Gravar Áudio</q-btn>
     <audio ref="audio" controls></audio>
     <q-btn @click="uploadFile">Upload arquivos sistema</q-btn>
     <input type="file" ref="fileInput" @change="handleFileUpload" style="display: none;" />
+    <ul>
+      <li v-for="file in selectedFiles" :key="file.name">{{ file.name }}</li>
+    </ul>
   </div>
 </template>
 
@@ -15,16 +20,39 @@ export default {
     return {
       mediaRecorder: null,
       audioChunks: [],
-      videoStream: null
+      videoChunks: [],
+      videoStream: null,
+      isRecording: false,
+      selectedFiles: []
     };
   },
   methods: {
-    async startCamera() {
+    async startRecordingVideo() {
       try {
         this.videoStream = await navigator.mediaDevices.getUserMedia({ video: true });
         this.$refs.video.srcObject = this.videoStream;
+        this.mediaRecorder = new MediaRecorder(this.videoStream);
+        this.mediaRecorder.ondataavailable = (event) => {
+          if (event.data.size > 0) {
+            this.videoChunks.push(event.data);
+          }
+        };
+        this.mediaRecorder.onstop = () => {
+          const videoBlob = new Blob(this.videoChunks, { type: 'video/webm' });
+          this.$refs.recordedVideo.src = URL.createObjectURL(videoBlob);
+          this.videoChunks = [];
+        };
+        this.mediaRecorder.start();
+        this.isRecording = true;
       } catch (error) {
-        console.error("Erro ao acessar a câmera:", error);
+        console.error("Erro ao iniciar a gravação de vídeo:", error);
+      }
+    },
+    stopRecordingVideo() {
+      if (this.mediaRecorder) {
+        this.mediaRecorder.stop();
+        this.videoStream.getTracks().forEach(track => track.stop());
+        this.isRecording = false;
       }
     },
     async startRecording() {
@@ -37,7 +65,7 @@ export default {
           }
         };
         this.mediaRecorder.onstop = () => {
-          const audioBlob = new Blob(this.audioChunks);
+          const audioBlob = new Blob(this.audioChunks, { type: 'audio/webm' });
           this.$refs.audio.src = URL.createObjectURL(audioBlob);
           this.audioChunks = [];
         };
@@ -50,13 +78,15 @@ export default {
       this.$refs.fileInput.click();
     },
     handleFileUpload(event) {
-      const file = event.target.files[0];
-      if (file) {
-        console.log("Arquivo selecionado:", file);
-        // Implementar o upload do arquivo para o servidor ou outra lógica necessária
+      const files = event.target.files;
+      if (files.length > 0) {
+        this.selectedFiles = Array.from(files);
+        console.log("Arquivos selecionados:", this.selectedFiles);
+        // Aqui você pode implementar o upload dos arquivos para o servidor ou outra lógica necessária
       }
     }
   }
 };
 </script>
+
 
